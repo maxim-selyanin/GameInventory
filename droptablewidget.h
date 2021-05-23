cd31ab88bb6aa4801e0a3a59d101db61466eec64
Maxim Selyanin <51035205+maxim-selyanin@users.noreply.github.com>
2021-05-23 19:31:49 +0300

    Update droptablewidget.h
    добавлены комментарии

--- droptablewidget.h
+++ droptablewidget.h
@@ -4,26 +4,37 @@
 #include <QTableWidget>
 #include "commondefinitions.h"
 
+///
+/// \brief реализует отображение инвентаря
+///
 class AppleDropTableWidget : public QTableWidget
 {
     Q_OBJECT
 
-    void playSound() const;
-
 protected:
+    //drop event
     virtual void dropEvent(QDropEvent *event) override;
+
+    //разрешаем эти ивенты, чтобы можно было принимать дроп ивенты
     virtual void dragEnterEvent(QDragEnterEvent *event) override;
     virtual void dragMoveEvent(QDragMoveEvent *event) override;
 
+    //здесь происходит удаление яблок и драг ивент в другие клетки
     virtual void mousePressEvent(QMouseEvent *event) override;
 
 public:
     explicit AppleDropTableWidget(int rows, int columns, QWidget *parent = nullptr);
 
-signals:
-    void itemAddedOn(int row, int column, ItemType type, int amountOf);
-    void itemDeletedOn(int row, int column, ItemType type, int amountOf);
+public slots:
+    //играть звук
+    void playSound();
 
+signals:
+    //добавление айтема типа type в строку row, столбец column в количестве amountOf
+    void itemAddedOn(int row, int column, ItemType type, int amountOf);
+    //удаление айтема типа type из строки row, столбца column в количестве amountOf
+    void itemDeletedOn(int row, int column, ItemType type, int amountOf);
+    //перетаскивание всех айтемов из клетки fromRow x fromColumn в клетку toRow x toColumn
     void moveItemsFrom(int fromRow, int fromColumn, int toRow, int toColumn);
 };
 
