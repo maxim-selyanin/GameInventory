639d0710af66bbc982700045f4212648d87ade09
Maxim Selyanin <51035205+maxim-selyanin@users.noreply.github.com>
2021-05-23 19:29:14 +0300

    Update inventorywidget.h
    добавлены комментарии

--- inventorywidget.h
+++ inventorywidget.h
@@ -11,32 +11,45 @@ class InventoryWidget : public QWidget
 {
     Q_OBJECT
 
+    //строки-столбцы
     const int rows;
     const int columns;
 
+    //инвентарь per se
     AppleDropTableWidget *tableWidget = nullptr;
 
+    //создания инвентаря, яблочного виджета и кнопки главного меню
     AppleDropTableWidget* createTableWidget();
     DragAppleLabel* createAppleWidget();
     QPushButton* createMainMenuButton();
 
+    //pixmap с нарисованным числом amount справа внизу
     static QPixmap getPixmap(const QPixmap &pixmap, int amount);
+
+    //параметры текста на вышеописанной пиксмапе
     static constexpr int textHorizontalOffset = 20;
     static constexpr int textVerticalOffset = 20;
     static constexpr int textSize = 10;
 
 public:
     explicit InventoryWidget(int rows_, int columns_, QWidget *parent = nullptr);
-    ~InventoryWidget();
+    ~InventoryWidget() override;
 
 public slots:
+    //в клетке row x column стало newAmount предметов с картинкой pic
     void changedAmountOn(int row, int column, const QPixmap &pic, int newAmount);//
 
 signals:
-    void showMainMenu();//
-    void itemAddedOn(int row, int column, ItemType type, int amountOf);//
-    void itemDeletedOn(int row, int column, ItemType type, int amountOf);//
+    //показать главное меню
+    void showMainMenu();
+    //в клетку row x column добавили amountOf предметов типа type
+    void itemAddedOn(int row, int column, ItemType type, int amountOf);
+    //из клетки row x column удалили amountOf предметов типа type
+    void itemDeletedOn(int row, int column, ItemType type, int amountOf);
+    //из клетки fromRow x fromColumn переместили все предметы в клетку toRow x toColumn
     void moveItemsFrom(int fromRow, int fromColumn, int toRow, int toColumn);
+    //нужно воспроизвести звук
+    void playSound();
 };
 
 #endif // INVENTORYWIDGET_H
