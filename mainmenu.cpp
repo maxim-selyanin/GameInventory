c037659050135347a7f9c38be08385f1f0d4511e
Maxim Selyanin <51035205+maxim-selyanin@users.noreply.github.com>
2021-05-23 19:26:34 +0300

    Update mainmenu.cpp
    кнопки расставлены на гриде по горизонтали, а не по вертикали

--- mainmenu.cpp
+++ mainmenu.cpp
@@ -6,28 +6,24 @@
 MainMenu::MainMenu(QWidget *parent)
     : QWidget(parent)
 {
+    //кнопочки
     QPushButton *newGameBtn = new QPushButton("New Game", this);
     QPushButton *exitBtn = new QPushButton("Exit", this);
 
     QGridLayout *layout = new QGridLayout(this);
 
+    //кидаем кнопочки на сетчатый лейаут
     layout->addWidget(newGameBtn, 0, 0);
-    layout->addWidget(exitBtn, 1, 0);
+    layout->addWidget(exitBtn, 0, 1);
 
     this->setLayout(layout);
 
+    //коннектим сигналы
     connect(newGameBtn, &QPushButton::pressed, this, &MainMenu::newGame);
-    connect(exitBtn, &QPushButton::pressed, this, &MainMenu::closed);
+    connect(exitBtn, &QPushButton::pressed, this, &MainMenu::exitSignal);
     connect(exitBtn, &QPushButton::pressed, this, &QWidget::close);
 }
 
 MainMenu::~MainMenu()
 {
 }
-
-void MainMenu::closeEvent(QCloseEvent *event)
-{
-    emit closed();
-    event->accept();
-}
-
