3f563d976f58d0c65113db7f15c39e8ff5c66bcd
Maxim Selyanin <51035205+maxim-selyanin@users.noreply.github.com>
2021-05-23 19:26:54 +0300

    Update main.cpp
    добавлено нахождение директории, в которой выполняется приложение

--- main.cpp
+++ main.cpp
@@ -1,10 +1,39 @@
 #include "application.h"
+#include <QSqlDatabase>
+#include <QDebug>
+#include <QSqlError>
+#include <QDir>
 
 constexpr int rows = 3;
 constexpr int columns = 3;
+constexpr char databaseRelativePath[] = "/databases/gamedata";
+
+/*
+ * таблица gamedata создана следующими командами:
+
+create table items (
+    item_id integer primary key not null
+    , item_name text
+    , item_pic_path text
+);
+
+create table inventory (
+    item_id integer
+    , row integer
+    , col integer
+    , amount integer
+    , foreign key (item_id) references items(item_id)
+);
+
+*/
 
 int main(int argc, char *argv[])
 {
-    Application a(rows, columns, argc, argv);
+    //находим, где лежит база данных
+    QDir dir{QDir::currentPath()};
+    dir.cdUp();
+    dir.cd("GameInventory");
+    //запускаем приложение
+    Application a(dir.path() + databaseRelativePath, rows, columns, argc, argv);
     return a.exec();
 }
