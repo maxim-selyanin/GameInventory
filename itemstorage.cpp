182d80a8bef7a502eca844c8d03ac6d13a406f32
Maxim Selyanin <51035205+maxim-selyanin@users.noreply.github.com>
2021-05-23 19:27:41 +0300

    Update itemstorage.cpp

--- itemstorage.cpp
+++ itemstorage.cpp
@@ -1,16 +1,18 @@
 #include "itemstorage.h"
 
-void addItemToGlobalStorage(GlobalItemStorage &storage, ItemType type)
+void addItemToGlobalStorage(GlobalItemStorage &storage, DatabaseHolder &db, ItemType type)
 {
     storage[type] = Item(type).getPixmap();
+    db.addNewItemType(type);
 }
 
-void addAllItemsToGlobalStorage(GlobalItemStorage &storage)
+void addAllItemsToGlobalStorage(GlobalItemStorage &storage, DatabaseHolder &db)
 {
+    //пробегаемся в свитче по всем имеющимся типам
     ItemType dummy = ItemType::apple;
     switch (dummy) {
     case ItemType::apple: {
-        addItemToGlobalStorage(storage, ItemType::apple);
+        addItemToGlobalStorage(storage, db, ItemType::apple);
     }//no break to add all existing items
     }
 }
