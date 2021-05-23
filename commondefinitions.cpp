a0b671483aa3d4a3a0f65ab55531b80c26efc519
Maxim Selyanin <51035205+maxim-selyanin@users.noreply.github.com>
2021-05-23 19:34:03 +0300

    Update commondefinitions.cpp
    Добавлены функции выдачи текстового представления типа и пути к его картинке

--- commondefinitions.cpp
+++ commondefinitions.cpp
@@ -6,3 +6,17 @@ QString getStringMarker(ItemType type)
     result.setNum(static_cast<ItemTypeStorage>(type));
     return result;
 }
+
+QString getTypeAsString(ItemType type)
+{
+    switch (type) {
+    case ItemType::apple: return "apple";
+    }
+}
+
+QString getPicPath(ItemType type)
+{
+    switch (type) {
+    case ItemType::apple: return smallApplePath;
+    }
+}
