e38af43488e52646d0c03b28f62e00a16dbecd49
Maxim Selyanin <51035205+maxim-selyanin@users.noreply.github.com>
2021-05-23 19:27:28 +0300

    Update itemstorage.h
    добавление всех имеющихся типов в GlobalStorage и DatabaseHolder

--- itemstorage.h
+++ itemstorage.h
@@ -3,10 +3,16 @@
 
 #include "item.h"
 #include <QMap>
+#include "database.h"
 
+///
+/// \brief хранилище всех типов предметов и их картинок
+///
 using GlobalItemStorage = QMap<ItemType, QPixmap>;
 
-void addItemToGlobalStorage(GlobalItemStorage &storage, ItemType type);
-void addAllItemsToGlobalStorage(GlobalItemStorage &storage);
+//добавить предмет типа type в хранилище и в табличку items базы данных
+void addItemToGlobalStorage(GlobalItemStorage &storage, DatabaseHolder &db, ItemType type);
+//добавить все имеющиеся типы предметов в хранилище и в табличку items базы данных
+void addAllItemsToGlobalStorage(GlobalItemStorage &storage, DatabaseHolder &db);
 
 #endif // ITEMSTORAGE_H
